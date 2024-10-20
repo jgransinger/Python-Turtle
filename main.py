21cbbb90d4c2ee20c78454d10a547223d99d467d
Joe Gran <132323475+jgransinger@users.noreply.github.com>
2024-10-20 13:40:09 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import turtle
+
+slowmo = turtle.Turtle()
+slowmo.shape("turtle")
+slowmo.color("green")
+slowmo.forward(100)
+slowmo.right(90)
+slowmo.forward(100)
+slowmo.right(90)
+slowmo.forward(100)
+slowmo.right(90)
+slowmo.forward(100)
+
+
+
+myScreen = turtle.Screen()
+myScreen.exitonclick()
+
+
